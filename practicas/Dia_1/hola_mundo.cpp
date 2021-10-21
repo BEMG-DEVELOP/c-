e4aebf792ab02e363d45500f3bcb52bf1ea81c32
brian <al221711721@gmail.com>
2021-10-21 16:28:34 -0500

    Primer día

---
+++ practicas/Dia_1/hola_mundo.cpp
@@ -0,0 +1,13 @@
+/*
+    Creando un Hola Mundo  con c++ para inciar a aprender.
+    Autor: Ing. Brian E. Martinez Garcia
+*/
+#include <iostream> //Biblioteca donde se encuentra la funcion cout
+
+    using namespace std; // Uso del espacio de nombre std
+
+    int main(){     //inicio de la funcion main
+ 
+        std::cout << "Hola mundo, Mi nombre es Brian";   //Imprimir en la console
+        return 1;                   // al terminar retorna 1
+    }
